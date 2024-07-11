0cafad31e7105013fb327cd0c28fdfea7cfa4995
RRambo <33575029+RRambo@users.noreply.github.com>
2024-07-11 18:12:40 +0300

    Add files via upload

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './2.17 App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
